3f4e4a992b1ca09f66ad320c7ad9a2c4ad06edda
adriano.exe <58786675+adriano@users.noreply.github.com>
2020-05-26 19:18:53 -0300

    novo program com funcao

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nomeaocontrario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+
+
+            Console.Write("Digite seu nome: ");
+            nome = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("Seu nome: " + ReverterNome(nome));
+
+            Console.Read();
+        }
+        public static string ReverterNome(string nome)
+        {
+            char[] digNome = nome.ToCharArray();
+            Array.Reverse(digNome);
+            return new string (digNome);
+        }
+    }
+}
